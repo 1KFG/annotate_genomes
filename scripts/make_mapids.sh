b9cd874d27c1eda9ae9e52fd5f02b50c3093027c
Jason Stajich <jason@bioperl.org>
2016-04-18 06:27:11 -0700

    revise for the updated cluster

--- scripts/make_mapids.sh
+++ scripts/make_mapids.sh
@@ -1,12 +1,15 @@
 #PBS -j oe
-module load stajichlab
-module load stajichlab-perl
-module load perl
+module unload perl
+module unload perl
+module unload perl
 module load maker/2.31.8
-
+INFILE=$1
 D=`dirname \`pwd\``
-SPECIES=`basename $D`
+SPECIES=`basename \`pwd\``
 PREF=`echo $SPECIES | perl -p -e 'my @n = split('_',$_); $_ = uc substr($n[0],0,1) . substr($n[1],0,3). "_"'`
-
+echo $D
 echo $PREF
-maker_map_ids  --prefix $PREF --iterate 0 --justify 5 $SPECIES.all.gff > $SPECIES.mapids
+if [ ! $INFILE ]; then
+INFILE=$SPECIES.all.gff
+fi
+maker_map_ids  --prefix $PREF --iterate 0 --justify 5 $INFILE > $SPECIES.mapids
--- scripts/map_ids.sh
+++ scripts/map_ids.sh
@@ -1,12 +1,11 @@
 #PBS -j oe
-module load stajichlab
-module load stajichlab-perl
-module load perl
-module load maker/2.31.6
+module unload perl/5.22.0
+module load perl/5.20.2
+module load maker
 
 D=`dirname \`pwd\``
 SPECIES=`basename $D`
-
+echo $SPECIES
 map_fasta_ids $SPECIES.mapids $SPECIES.all.maker.proteins.fasta
 map_fasta_ids $SPECIES.mapids $SPECIES.all.maker.transcripts.fasta
 map_gff_ids $SPECIES.mapids $SPECIES.all.gff 

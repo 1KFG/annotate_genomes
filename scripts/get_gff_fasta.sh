c4eb7767dbdc378770414843a5cec08692744477
Jason Stajich <jason@bioperl.org>
2016-04-24 14:26:36 -0700

    updated modules, don't guess logfile in any special way, assumes 1 run per folder

--- scripts/get_gff_fasta.sh
+++ scripts/get_gff_fasta.sh
@@ -1,10 +1,5 @@
 #PBS -l nodes=1:ppn=1 -j oe
-
-module load perl
-module load stajichlab
 module load maker/2.31.8
-D=`dirname \`pwd\``
-SPECIES=`basename $D`
 
-fasta_merge -d $SPECIES.maker.output/$SPECIES"_master_datastore_index.log"
-gff3_merge -d $SPECIES.maker.output/$SPECIES"_master_datastore_index.log"
+fasta_merge -d */*"_master_datastore_index.log"
+gff3_merge -d */*"_master_datastore_index.log"

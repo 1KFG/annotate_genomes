62b666e6ee9958d6ea1b1501d1b976afd2cceb19
Jason Stajich <jason@bioperl.org>
2016-07-25 08:41:59 -0700

    run as array

--- scripts/run_swissprot_blast.sh
+++ scripts/run_swissprot_blast.sh
@@ -2,5 +2,5 @@
 module load ncbi-blast
 D=`dirname \`pwd\``
 SPECIES=`basename $D`
-blastp -query $SPECIES.all.maker.proteins.fasta -db /shared/stajichlab/db/uniprot/uniprot_sprot.fasta -use_sw_tback \
+blastp -query $SPECIES.all.maker.proteins.fasta -db /bigdata/stajichlab/shared/db/uniprot/uniprot_sprot.fasta -use_sw_tback \
 -num_threads $PBS_NP -outfmt 6 -evalue 1e-3 -out $SPECIES.all.maker.swissprot.BLASTP -max_target_seqs 5
